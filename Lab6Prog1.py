bb942480e470e331671d4fd21ea7d04fcf658f74
rileytromer <rileytromer@ufl.edu>
2023-10-25 18:00:27 -0400

    added stuff

--- Lab6Prog1.py
+++ Lab6Prog1.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+code = True
+while code:
+  print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+  print("Please Enter an option: ", end="")
+  option_number = int(input())
+  if option_number == 1:
+      # encode
+  if option_number == 2:
+      # decode
+  if option_number == 3:
+    code = False
